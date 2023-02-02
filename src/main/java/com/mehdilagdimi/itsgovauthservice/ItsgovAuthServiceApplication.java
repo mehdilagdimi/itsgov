bd094d650fac843afca2efd482a6370fdd277026
mehdilagdimi <lagdimi.mehdi@gmail.com>
2023-02-02 11:53:49 +0100

    first init auth ser

---
+++ src/main/java/com/mehdilagdimi/itsgovauthservice/ItsgovAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mehdilagdimi.itsgovauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsgovAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsgovAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mehdilagdimi/itsgovauthservice/ItsgovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdilagdimi.itsgovauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsgovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
